1f3a3c94116e02f65b863eab764bbe5b331baa3f
Timo de Kort <dekort.timo@gmail.com>
2020-09-20 19:22:09 +0200

    Add fetch_32 to bios

--- src/bios.rs
+++ src/bios.rs
@@ -19,4 +19,13 @@ impl Bios {
 
         bios
     }
+
+    pub fn fetch_32(&self, offset: u32) -> u32 {
+        let offset = offset as usize;
+
+        self.data[offset + 0] as u32 |
+        (self.data[offset + 1] as u32) << 8 |
+        (self.data[offset + 2] as u32) << 16 |
+        (self.data[offset + 3] as u32) << 24
+    }
 }
