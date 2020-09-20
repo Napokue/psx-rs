fe79d3d35e14341b893648abf087e2534a75dc57
Timo de Kort <dekort.timo@gmail.com>
2020-09-20 19:38:17 +0200

    add fetch_32 to system

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,5 @@
 use crate::bios::Bios;
+use crate::map;
 
 pub struct System {
     bios: Bios
@@ -8,6 +9,14 @@ impl System {
     pub fn new(bios: Bios) -> Self {
         System {
             bios
+        }
+    }
+
+    pub fn fetch_32(&self, address: u32) -> u32 {
+        if let Some(offset) = map::BIOS.contains(address) {
+            self.bios.fetch_32(offset)
+        } else {
+            panic!("Address could not be fetched")
         }
     }
 }
